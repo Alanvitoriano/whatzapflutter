13b7e9f31d3b630fe415d9af94d1a5777542c5c2
Alan vitoriano <69824299+Alanvitoriano@users.noreply.github.com>
2021-05-09 11:25:14 -0300

    commit

--- lib/Login.dart
+++ lib/Login.dart
@@ -8,6 +8,17 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("hello")));
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
